629efbc18fe45010076c0c106127599faa23bf25
ryandwi <r_y4ns@yahoo.com>
2020-08-26 05:45:28 +0000

    add custom Error csrf token mismatch

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const path = require('path')
 const bodyParser = require("body-parser")
 const csrf = require('csurf')
 const cookieParser = require('cookie-parser')
+const compression = require('compression')
 const PORT = 8080
 
-
+// COMPRESSION
+app.use(compression())
 
 // MIDDLEWARE
 const csrfProtection = csrf({ cookie: true })
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(csrfProtection)
+
+// MISMATCH TOKEN
+app.use(function (err, req, res, next) {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err)
+  res.status(403)
+    .json({ status: false, data: 'Error csrf token mismatch'})
+})
 
 //  SET VIEW
 app.set('views', path.join(__dirname, 'views'));
--- routers/email.js
+++ routers/email.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const { runValidation, validationRegister } = require('../validation/register-validation')
 
 const {
@@ -14,5 +15,18 @@ router
 router
     .route('/search')
     .post(validationRegister, runValidation, submitEmail)
+
+router.use(function(req, res, next) {
+    if (!req.route)
+        return res.status(404).json({
+            status : false,
+            data: 'Error 404'
+        })
+    next();
+});
+
+
+
+
 
 module.exports = router
